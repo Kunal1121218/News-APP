c3aa44bf0a23ef172bef0b18516548bb4495cbf2
Kunal Jadhav <112310504+Kunal1121218@users.noreply.github.com>
2022-12-26 12:24:40 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -24,11 +24,11 @@ export default function Navbar({selectedCountry}) {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              {/* <li className="nav-item">
+              {<li className="nav-item">
                 <Link className="nav-link active" aria-current="page" to="/">
                   Home
                 </Link>
-              </li> */}
+              </li>}
               <li className="nav-item">
                 <Link className="nav-link" to="/business">
                   Business
